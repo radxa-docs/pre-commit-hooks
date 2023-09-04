6ec151211ad814f84eb781288afe8a69e8ac08ab
ZHANG Yuntian <95260730+RadxaYuntian@users.noreply.github.com>
2023-09-04 20:09:44 +0800

    Convert non webp image

--- pre_commit_hooks/use-webp-image.sh
+++ pre_commit_hooks/use-webp-image.sh
@@ -8,6 +8,19 @@ check() {
     then
         echo "$1: forbidden image format. Use webp instead." >&2
         ret=1
+        if ! $DRY_RUN
+        then
+            local webp="${1%.*}.webp"
+            if [[ -f "$webp" ]]
+            then
+                echo "$1: found existing webp file. Will not attempt to fix this issue." >&2
+                return $ret
+            fi
+
+            cwebp "$1" -o "$webp" &>/dev/null
+            rm "$1"
+            grep -rl "/$(basename "$1")" . --exclude-dir=.git --exclude-dir=node_modules --exclude-dir=build | xargs sed -i -E 's|(!\[.+\]\(.*/)('"$(basename "$1")"')\)|\1'"$(basename "$webp")"')|g'
+        fi
     fi
 
     return $ret
